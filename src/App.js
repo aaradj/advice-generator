b62a84fa511751379e6600b660ab908169409441
Arad Jahangiri <aradjahangiri84@gmail.com>
2023-05-15 16:13:39 +0330

    DONE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Main from "./components/Main/Main";
+import "./app.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Main />
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
         const getData = async () =>{
             await axios.get("https://api.adviceslip.com/advice")
             .then(response=>{
-                setData(response.data.slip.advice)
+                setData(response.data.slip)
             })
             .catch(()=>{
                 console.log("somthing went wrong")
@@ -23,9 +23,11 @@ function Main() {
     },[click]);
 
   return (
-    <div>
-        <h1>{data}</h1>
-        <button onClick={handleClick}>get advice</button>
+    <div className={styles.card}>
+        <p>ADVICE  #{data.id && data.id}</p>
+        <h1>"{data.advice && data.advice}"</h1>
+        <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+        <button onClick={handleClick}><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></button>
     </div>
   )
 }
